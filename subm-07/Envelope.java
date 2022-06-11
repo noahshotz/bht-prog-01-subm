0e01a22e9abb98cbbf5634619664a2e44747eef4
noahshotz <max.heinrich@uusimaa.de>
2022-06-11 12:04:59 +0200

    configured sender method

--- subm-07/Envelope.java
+++ subm-07/Envelope.java
@@ -11,8 +11,69 @@ public class Envelope {
     String fromName = "BHT Berlin";
     String fromAddress = "Luxemburger Str. 10";
     String fromZipCodeAndCity = "13353 Berlin";
-    
+
+    // padding generator
+    public void padding(){
+        for (int x = 0; x < padding; x++) {
+            System.out.print(spaceChar);
+        }
+    }
+
+    // print upper and lower limit
+    public void printBorder() {
+        System.out.print(cornerChar);
+        for (int x = 0; x < width - 2; x++) {
+            System.out.print(borderHorizontalChar);
+        }
+        System.out.print(cornerChar + lineBreak);
+    }
+
+    public void emptyLine() {
+        System.out.print(borderVerticalChar);
+        for (int x = 0; x < width - 2; x++) {
+            System.out.print(spaceChar);
+        }
+        System.out.print(borderVerticalChar + lineBreak);
+    }
+
+    public void senderData() {
+        // from name
+        System.out.print(borderVerticalChar);
+        padding();
+        System.out.print(fromName);
+        for (int x = 0; x < width - (2*padding) - fromName.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+
+        // from adddress
+        System.out.print(borderVerticalChar);
+        padding();
+        System.out.print(fromAddress);
+        for (int x = 0; x < width - (2*padding) - fromAddress.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+
+        //from fromZipCodeAndCity
+        System.out.print(borderVerticalChar);
+        padding();
+        System.out.print(fromZipCodeAndCity);
+        for (int x = 0; x < width - (2*padding) - fromZipCodeAndCity.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+    }
+
     public void printEnvelope(Student[] toStudents) {
-        System.out.println("test @ envelope");
+        printBorder();
+        emptyLine();
+        senderData();
+        emptyLine();
+        emptyLine();
+        printBorder();
     }
 }
