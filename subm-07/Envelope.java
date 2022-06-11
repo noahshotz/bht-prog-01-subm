6185ced5c670685161136e8dd8d8a2ad8a957503
noahshotz <max.heinrich@uusimaa.de>
2022-06-11 15:42:17 +0200

    hmmmm

--- subm-07/Envelope.java
+++ subm-07/Envelope.java
@@ -12,6 +12,10 @@ public class Envelope {
     String fromAddress = "Luxemburger Str. 10";
     String fromZipCodeAndCity = "13353 Berlin";
 
+    String toName = "$NAME";
+    String toAddress = "$ADDRESS";
+    String toZipCodeAndCity = "$ZIP $CITY";
+    
     // padding generator
     public void padding(){
         for (int x = 0; x < padding; x++) {
@@ -68,11 +72,124 @@ public class Envelope {
         System.out.print(borderVerticalChar + lineBreak);
     }
 
+    public void receiverData() {
+        if (toName.length() > toAddress.length() && toName.length() > toZipCodeAndCity.length()) {
+            toNameIsLongest();
+        } else if (toAddress.length() > toName.length() && toAddress.length() > toZipCodeAndCity.length()) {
+            toAddressIsLongest();
+        } else if (toZipCodeAndCity.length() > toAddress.length() && toZipCodeAndCity.length() > toName.length()) {
+            toZipCodeAndCityIsLongest();
+        }
+    }
+
+    public void toNameIsLongest() {
+        // from name
+        System.out.print(borderVerticalChar);
+        padding();
+        for (int x = 0; x < width - (2*padding) - toName.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        System.out.print(toName);
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+
+        // from adddress
+        System.out.print(borderVerticalChar);
+        padding();
+        for (int x = 0; x < width - (2*padding) - toAddress.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        System.out.print(toAddress);
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+
+        //from fromZipCodeAndCity
+        System.out.print(borderVerticalChar);
+        padding();
+        for (int x = 0; x < width - (2*padding) - toZipCodeAndCity.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        System.out.print(toZipCodeAndCity);
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+    }
+
+    public void toAddressIsLongest() {
+        // from name
+        System.out.print(borderVerticalChar);
+        padding();
+        for (int x = 0; x < width - (2*padding) - toName.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        System.out.print(toName);
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+
+        // from adddress
+        System.out.print(borderVerticalChar);
+        padding();
+        for (int x = 0; x < width - (2*padding) - toAddress.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        System.out.print(toAddress);
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+
+        //from fromZipCodeAndCity
+        System.out.print(borderVerticalChar);
+        padding();
+        for (int x = 0; x < width - (2*padding) - toZipCodeAndCity.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        System.out.print(toZipCodeAndCity);
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+    }
+
+    public void toZipCodeAndCityIsLongest() {
+        // from name
+        System.out.print(borderVerticalChar);
+        padding();
+        for (int x = 0; x < width - (2*padding) - toName.length() - 2 - (toZipCodeAndCity.length() - toName.length()); x++){
+            System.out.print(spaceChar);
+        }
+        System.out.print(toName);
+        for (int x = 0; x < (toZipCodeAndCity.length() - toName.length()); x++) {
+            System.out.print(spaceChar);
+        }
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+
+        // from adddress
+        System.out.print(borderVerticalChar);
+        padding();
+        for (int x = 0; x < width - (2*padding) - toAddress.length() - 2 - (toZipCodeAndCity.length() - toAddress.length()); x++){
+            System.out.print(spaceChar);
+        }
+        System.out.print(toAddress);
+        for (int x = 0; x < (toZipCodeAndCity.length() - toAddress.length()); x++) {
+            System.out.print(spaceChar);
+        }
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+
+        //from fromZipCodeAndCity
+        System.out.print(borderVerticalChar);
+        padding();
+        for (int x = 0; x < width - (2*padding) - toZipCodeAndCity.length() - 2; x++){
+            System.out.print(spaceChar);
+        }
+        System.out.print(toZipCodeAndCity);
+        padding();
+        System.out.print(borderVerticalChar + lineBreak);
+    }
+
     public void printEnvelope(Student[] toStudents) {
         printBorder();
         emptyLine();
         senderData();
         emptyLine();
+        receiverData();
         emptyLine();
         printBorder();
     }
--- subm-07/Student.java
+++ subm-07/Student.java
@@ -1,6 +1,6 @@
 public class Student {
-
+    
     public Student(String prename, String surname, String street, String houseNumber, String zipCode, String city, String registrationNumber, String course) {
-        System.out.println("test @ student: " + prename + " " + surname);
+        // ??
     }
 }
