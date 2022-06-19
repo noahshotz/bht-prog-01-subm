601f44f309fadaed49f7963f81c42b60d3a414fd
noahshotz <max.heinrich@uusimaa.de>
2022-06-19 14:51:54 +0200

    files added

---
+++ subm-08/student/studentEnum.java
@@ -0,0 +1,14 @@
+package student;
+
+public class studentEnum {
+
+    enum StudyCourse {
+        MEDIENINFORMATIK,
+        MEDIENINFORMATIK_ONLINE,
+        TECHNISCHE_INFORMATIK,
+        DRUCK_UND_MEDIENTECHNIK,
+        SCREEN_BASED_MEDIA
+    }
+
+    
+}
