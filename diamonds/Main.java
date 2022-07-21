d4bde7954ef2b22600de615ef21091b30d53a45d
noahshotz <max.heinrich@uusimaa.de>
2022-07-21 23:27:51 +0200

    diamond done

--- diamonds/Main.java
+++ diamonds/Main.java
@@ -1,12 +1,34 @@
 public class Main {
-    public static void main(String args[]) {
-        String words[] = {"Sonne", "Mond", "Stern", "PARTYS", "CHILLEN", "TANZEN", "Apfel", "Birne", "Sanddorn"};
+    static int diamondSize = 5;
 
-        for (int x = 0; x < words.length; x++) {
-            String currentIndex = words[x];
-            if (currentIndex.substring(currentIndex.length() - 1).equalsIgnoreCase("n")) {
-                System.out.println(x);
+    public static void main(String args[]) {
+
+        // top half of diamond
+        for (int i = 0; i <= diamondSize; i++) {
+            if (i % 2 != 0) {
+                printDiamond(i);
             }
         }
+
+        // bottom half of diamond
+        for (int i = diamondSize; i >= 0; i--) {
+            if (i % 2 != 0) {
+                printDiamond(i);
+            }
+        }
+    }
+
+    public static void printDiamond(int i) {
+        int x = (diamondSize - i) / 2;
+        for (int z = 0; z <= x; z++) {
+            System.out.print(" ");
+        }
+        for (int y = 0; y <= i - 1; y++) {
+            System.out.print("*");
+        }
+        for (int z = 0; z <= x; z++) {
+            System.out.print(" ");
+        }
+        System.out.print("\n");
     }
 }
